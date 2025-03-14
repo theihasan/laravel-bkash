a9c95fca582e686895557539b6ee4f7bf6a7ee04
theihasan <142471724+theihasan@users.noreply.github.com>
2025-03-14 18:05:10 +0000

    Fix styling

--- src/Exceptions/PaymentCreateException.php
+++ src/Exceptions/PaymentCreateException.php
@@ -4,7 +4,7 @@ namespace Ihasan\Bkash\Exceptions;
 
 class PaymentCreateException extends \Exception
 {
-    public function __construct($message = "Payment Creation Failed", $code = 500, \Throwable $previous = null)
+    public function __construct($message = 'Payment Creation Failed', $code = 500, ?\Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
--- src/Exceptions/PaymentExecuteException.php
+++ src/Exceptions/PaymentExecuteException.php
@@ -4,7 +4,7 @@ namespace Ihasan\Bkash\Exceptions;
 
 class PaymentExecuteException extends \Exception
 {
-    public function __construct($message = "Payment Execution Failed", $code = 500, \Throwable $previous = null)
+    public function __construct($message = 'Payment Execution Failed', $code = 500, ?\Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
